c01e0f305712289a005c3863b14793d4228ddb1d
Jkorf <jankorf91@gmail.com>
2026-02-16 10:21:36 +0100

    Fixed SymbolOrderBook websocket subscription not getting closed if when waiting for initial data times out

--- Polymarket.Net/SymbolOrderBooks/PolymarketClobSymbolOrderBook.cs
+++ Polymarket.Net/SymbolOrderBooks/PolymarketClobSymbolOrderBook.cs
@@ -74,6 +74,9 @@ namespace Polymarket.Net.SymbolOrderBooks
             Status = OrderBookStatus.Syncing;
 
             var setResult = await WaitForSetOrderBookAsync(_initialDataTimeout, ct).ConfigureAwait(false);
+            if (!setResult)
+                await result.Data.CloseAsync().ConfigureAwait(false);
+
             return setResult ? result : new CallResult<UpdateSubscription>(setResult.Error!);
         }
 
