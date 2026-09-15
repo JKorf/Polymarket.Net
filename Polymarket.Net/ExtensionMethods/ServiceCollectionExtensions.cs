f22ba6705764a1e30d5e52f0bbc154da33bf16a5
Jkorf <jankorf91@gmail.com>
2026-09-15 15:12:35 +0200

    wip

--- Polymarket.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Polymarket.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -33,29 +33,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            var options = new PolymarketOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-
-            try
-            {
-                configuration.Bind(options);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException("Invalid configuration provided", ex);
-            }
-
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            var restEnvName = options.Rest.Environment?.Name ?? options.Environment?.Name ?? PolymarketEnvironment.Live.Name;
-            var socketEnvName = options.Socket.Environment?.Name ?? options.Environment?.Name ?? PolymarketEnvironment.Live.Name;
-            options.Rest.Environment = PolymarketEnvironment.GetEnvironmentByName(restEnvName) ?? options.Rest.Environment!;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = PolymarketEnvironment.GetEnvironmentByName(socketEnvName) ?? options.Socket.Environment!;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
+            var options = PolymarketOptions.CreateFromConfiguration(configuration);
 
 
             services.AddSingleton(x => Options.Options.Create(options.Rest));
@@ -74,18 +52,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             Action<PolymarketOptions>? optionsDelegate = null)
         {
-            var options = new PolymarketOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-            optionsDelegate?.Invoke(options);
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            options.Rest.Environment = options.Rest.Environment ?? options.Environment ?? PolymarketEnvironment.Live;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = options.Socket.Environment ?? options.Environment ?? PolymarketEnvironment.Live;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
+            var options = PolymarketOptions.Create(optionsDelegate);
 
             services.AddSingleton(x => Options.Options.Create(options.Rest));
             services.AddSingleton(x => Options.Options.Create(options.Socket));
